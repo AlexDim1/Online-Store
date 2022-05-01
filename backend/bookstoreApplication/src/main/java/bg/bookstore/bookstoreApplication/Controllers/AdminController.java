c2cb6797c049c0e6670612a07a440cc52e11169e
Alexander Dimitrov <72303198+AlexDim1@users.noreply.github.com>
2022-05-01 15:22:04 +0300

    Added admin/products endpoint

--- backend/bookstoreApplication/src/main/java/bg/bookstore/bookstoreApplication/Controllers/AdminController.java
+++ backend/bookstoreApplication/src/main/java/bg/bookstore/bookstoreApplication/Controllers/AdminController.java
@@ -2,12 +2,16 @@ package bg.bookstore.bookstoreApplication.Controllers;
 
 import bg.bookstore.bookstoreApplication.Entities.Product;
 import bg.bookstore.bookstoreApplication.Payload.Request.AddProductRequest;
+import bg.bookstore.bookstoreApplication.Payload.Response.AdminProductResponse;
 import bg.bookstore.bookstoreApplication.Repositories.OrderRepository;
 import bg.bookstore.bookstoreApplication.Repositories.ProductRepository;
 import bg.bookstore.bookstoreApplication.Repositories.ReviewRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -21,6 +25,17 @@ public class AdminController {
         this.productRepo = productRepo;
         this.reviewRepo = reviewRepo;
         this.orderRepo = orderRepo;
+    }
+
+    @GetMapping("/products")
+    public List<AdminProductResponse> getProducts() {
+        List<Product> products = productRepo.findAll();
+
+        List<AdminProductResponse> response = new ArrayList<>();
+        for (Product product: products)
+            response.add(new AdminProductResponse(product));
+
+        return response;
     }
 
     @PostMapping("/products/add")
---
+++ backend/bookstoreApplication/src/main/java/bg/bookstore/bookstoreApplication/Payload/Response/AdminProductResponse.java
@@ -0,0 +1,89 @@
+package bg.bookstore.bookstoreApplication.Payload.Response;
+
+import bg.bookstore.bookstoreApplication.Entities.Product;
+
+public class AdminProductResponse {
+    private Long id;
+    private String name;
+    private String author;
+    private String description;
+    private String shortDescription;
+    private Integer pageCount;
+    private Double price;
+    private Integer timesBought;
+
+    public AdminProductResponse(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.author = product.getAuthor();
+        this.description = product.getDescription();
+        this.shortDescription = product.getDescription();
+        this.pageCount = product.getPageCount();
+        this.price = product.getPrice();
+        this.timesBought = product.getTimesBought();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public Integer getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(Integer pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getTimesBought() {
+        return timesBought;
+    }
+
+    public void setTimesBought(Integer timesBought) {
+        this.timesBought = timesBought;
+    }
+}
